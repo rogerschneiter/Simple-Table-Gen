7723d7d9b54322092cb97bd8d493e606524bd1f1
rogerschneiter <roger.schneiter@gmail.com>
2016-08-25 13:08:47 +0200

    Paths modified to work on all operating systems

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.omg.CORBA.Environment;
+
+import java.io.File;
+import java.nio.file.FileSystem;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Image icon = new Image(getClass().getResourceAsStream("\\res\\icon.png"));
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
         primaryStage.getIcons().addAll(icon);
         primaryStage.setTitle("Simple Table Generator V 1.1");
         primaryStage.resizableProperty().setValue(Boolean.FALSE);
         Scene s = new Scene(root, 750, 500);
-        s.getStylesheets().add(getClass().getResource("\\res\\style.css").toExternalForm());
+        s.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         primaryStage.setScene(s);
         primaryStage.show();
